512c1a22dfde94ebfb7500601d7a33891d1bd560
dok128 <dok8259@sookmyung.ac.kr>
2024-06-30 20:59:40 +0900

    내 서재 완료

--- src/components/MainScreen/SearchBar.jsx
+++ src/components/MainScreen/SearchBar.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import search from "assets/img/ico/search.ico";
+
 const SearchBar = () => {
+  const [searchBtn, setSearchBtn] = useState("");
+  const onSearchChange = (e) => {
+    setSearchBtn(e.target.value);
+  };
+
+  /* 검색 필터 - includes가 대소문자를 구분해서 소문자로 통일시켜줌
+               - replace로 공백도 없애서 검색에 이상 없도록 함
+  const filterTitle = books.filter((p) => {
+    return p.title.replace(" ", "").toLocaleLowerCase().includes(search.toLocaleLowerCase().replace(" ", ""))
+    })
+*/
   return (
     <div id="searchbar">
       <div className="search">
         <input
           className="searchInput"
           type="text"
+          value={searchBtn}
+          onChange={onSearchChange}
           placeholder="검색어를 입력해주세요."
         />
         <button className="searchbtn">
--- src/routes/My/MyLib.jsx
+++ src/routes/My/MyLib.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import Header2 from "components/Header/Header2";
+import boogi2 from "assets/img/boogi2.jpg";
+import { useState } from "react";
 
 const MyLib = () => {
+  const [activeTab, setActiveTab] = useState("관심 분야");
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "관심 분야":
+        return <div>관심 분야 내용</div>;
+      case "책장":
+        return <div>책장 내용</div>;
+      case "My Favorite":
+        return <div>My Favorite 내용</div>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <Header2 />
+      <div id="mylib">
+        <div className="profile_container">
+          <span>
+            <img src={boogi2} alt="프로필" className="profile_img"></img>
+          </span>
+          <span className="profile_title">
+            ㅇㅇㅇ<span>의 서재</span>
+          </span>
+        </div>
+        <div>
+          <ul className="mylib_menu">
+            {["관심 분야", "책장", "My Favorite"].map((tab) => (
+              <li
+                key={tab}
+                className={tab === activeTab ? "active" : ""}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="tab_content">{renderContent()}</div>
+      </div>
     </>
   );
 };
