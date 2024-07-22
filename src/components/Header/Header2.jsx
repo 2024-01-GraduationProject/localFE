203dcd351928cb5c48987b56516b3f193f4d35e7
dok128 <dok8259@sookmyung.ac.kr>
2024-07-22 23:25:28 +0900

    로그아웃 post 요청

--- src/components/Header/Header2.jsx
+++ src/components/Header/Header2.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "assets/img/logo.jpg";
+import api from "../../api";
 
 const Header2 = () => {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("email");
-    // 페이지 이동
-    navigate("/");
+  const handleLogout = async () => {
+    try {
+      const response = await api.post(
+        "/logout",
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        // 로그아웃 성공
+        sessionStorage.removeItem("token");
+        sessionStorage.removeItem("email");
+        // 페이지 이동
+        navigate("/");
+      } else {
+        // 로그아웃 실패
+        console.error("Logout failed");
+        console.log("로그아웃에 실패했습니다. 다시 시도해주세요.");
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
+      console.log("로그아웃 중 에러가 발생했습니다. 다시 시도해주세요.");
+    }
   };
 
   const handleNavigate = (path) => {
