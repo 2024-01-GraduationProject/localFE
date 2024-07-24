0ff2170099c2e5a2c44601e9936b7aab81639d50
dok128 <dok8259@sookmyung.ac.kr>
2024-07-24 13:47:34 +0900

    쿠키 허용으로 수정

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080", // 포트 번호 8080으로 설정
+  withCredentials: true, // 세션 쿠키를 전송
 
   headers: {
     "Content-Type": "application/json",
